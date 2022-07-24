8d6d27d851d0f73c84644295936c2b6b88db65b1
daheige <zhuwei313@hotmail.com>
2022-07-24 19:31:54 +0800

    feat: function protype params

--- part1/func_interface/func.c
+++ part1/func_interface/func.c
@@ -4,6 +4,19 @@
 // 返回值类型 function名字 参数0个或多个
 // int power(int m, int n);
 int power(int, int); // 原型定义中的参数名字可选
+
+int power2(int base, int n)
+{
+    // 减少局部变量声明
+    int p;
+    for (p = 1; n > 0; --n)
+    {
+        p = p * base;
+    }
+
+    return p;
+}
+
 int main()
 {
 
@@ -12,6 +25,8 @@ int main()
     {
         printf("current index = %d power(2,%d) = %d power(-3,%d) = %d\n", i, i, power(2, i), i, power(-3, i));
     }
+
+    printf("power2(2,1) = %d\n", power2(2, 1));
 }
 
 // power函数声明实现
